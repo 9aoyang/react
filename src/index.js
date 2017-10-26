b64bfd428885967f5679cb819339a90afe9b1840
9aoyang <9aoyang@dxy.cn>
2017-10-26 14:29:09 +0800

    learn react component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,63 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+class Title extends Component {
+  handleClickOnTitle (word, e) {
+    console.log (this, word)
+  }
+
+  render () {
+    return (
+      <h1 onClick={this.handleClickOnTitle.bind(this, 'hello')}>He1lo React</h1>
+    )
+  }
+}
+
+class Header extends Component {
+  render () {
+    return (
+      <div>
+        <Title />
+        <h2>This is Header</h2>
+      </div>
+    )
+  }
+}
+
+class Main extends Component {
+  render () {
+    return (
+      <div>
+        <h2>This is main content</h2>
+      </div>
+    )
+  }
+}
+
+class Footer extends Component {
+  render () {
+    return (
+      <div>
+        <h2>This is footer</h2>
+      </div>
+    )
+  }
+}
+
+class Index extends Component {
+  render () {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <Index />,
+  document.getElementById('root')
+)
