934de259a8546c864690bc4ea0cb3c82b5ad2c16
9aoyang <9aoyang@dxy.cn>
2017-10-30 14:31:14 +0800

    autofocus

--- src/index.js
+++ src/index.js
@@ -2,43 +2,20 @@ import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-const users = [
-  { username: 'Jerry', age: 21, gender: 'male' },
-  { username: 'Tomy', age: 22, gender: 'male' },
-  { username: 'Lily', age: 19, gender: 'female' },
-  { username: 'Lucy', age: 20, gender: 'female' }
-]
-
-class User extends Component {
-  render () {
-    const { user: {
-      username,
-      age,
-      gender
-    } } = this.props
-    return (
-      <div>
-        <div>name: {username}</div>
-        <div>age: {age}</div>
-        <div>gender: {gender}</div>
-        <hr/>
-      </div>
-    )
+class AutoFocusInput extends Component {
+  componentDidMount () {
+    this.input.focus()
   }
-}
 
-class Index extends Component {
   render () {
     return (
-      <div>
-        { users.map( (user, i) => <User key={i} user={user}/> ) }
-      </div>
+      <input ref={(input) => this.input = input}/>
     )
   }
 }
 
 ReactDOM.render(
-  <Index />,
+  <AutoFocusInput />,
   document.getElementById('root')
 )
 
