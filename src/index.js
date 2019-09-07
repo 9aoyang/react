ed33edd258aa81ccdfde9f2bb8cf8efd22babd80
9aoyang <9aoyang@gmail.com>
2019-09-07 13:52:16 +0800

    build: initial repo

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import ReactDOM from 'react-dom'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-class Comment extends Component {
-  static propTypes = {
-    comment: PropTypes.object.isRequired
-  }
+ReactDOM.render(<App />, document.getElementById('root'));
 
-  render () {
-    const { comment } = this.props
-    return (
-      <div className='comment'>
-        <div className='comment-user'>
-          <span>{comment.username} </span>
-        </div>
-        <p>{comment.content}</p>
-      </div>
-    )
-  }
-}
-
-ReactDOM.render(
-  <Comment />
-  ,document.getElementById('root')
-)
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
